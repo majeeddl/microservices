6e8bfa8f71d36c1bab9a34c21a560f250255d0eb
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-12-01 17:43:44 +0300

    add new property for Shopping Card

--- DotNetCore-Microservices/src/Basket.API/Domains/Entities/ShoppingCard.Entity.cs
+++ DotNetCore-Microservices/src/Basket.API/Domains/Entities/ShoppingCard.Entity.cs
@@ -6,9 +6,22 @@
 
         public List<ShoppingCardItem> Items { get; set; } = new List<ShoppingCardItem>();
 
+
+        public ShoppingCard()
+        {
+            
+        }
         public ShoppingCard(string username)
         {
             Username = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Items.Sum(item => item.Price + item.Quantity);
+            }
         }
     }
 }
