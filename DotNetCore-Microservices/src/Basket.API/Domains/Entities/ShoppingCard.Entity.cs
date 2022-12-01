7c086a531a6b38fdc6506fa7b82b076ef03b8ee8
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-12-01 17:37:42 +0300

    create new entities

---
+++ DotNetCore-Microservices/src/Basket.API/Domains/Entities/ShoppingCard.Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Domains.Entities
+{
+    public class ShoppingCard
+    {
+        public string Username { get; set; }
+
+        public List<ShoppingCardItem> Items { get; set; } = new List<ShoppingCardItem>();
+
+        public ShoppingCard(string username)
+        {
+            Username = username;
+        }
+    }
+}
---
+++ DotNetCore-Microservices/src/Basket.API/Domains/Entities/ShoppingCardItem.Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Domains.Entities;
+
+public class ShoppingCardItem
+{
+    public int Quantity { get; set; }
+
+    public string Color{ get; set; }
+
+    public decimal Price { get; set; }
+
+    public string ProductId { get; set; }
+    public string ProductName { get; set; }
+}
