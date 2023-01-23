7381ea56019986499b0afcabb0c76de111c0acb9
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-01-23 12:01:18 +0300

    fix ports

--- DotNetCore-Microservices/src/Discount.API/Program.cs
+++ DotNetCore-Microservices/src/Discount.API/Program.cs
@@ -1,11 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
 // Add services to the container.
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
