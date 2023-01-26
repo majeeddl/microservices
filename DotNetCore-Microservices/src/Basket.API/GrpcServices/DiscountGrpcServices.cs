70997838c75eb6d7eda638d6f36fbb84ea86a1d0
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-01-26 12:37:54 +0300

    consuming discount grpc service into basket api

---
+++ DotNetCore-Microservices/src/Basket.API/GrpcServices/DiscountGrpcServices.cs
@@ -0,0 +1,26 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcServices;
+
+
+public class DiscountGrpcServices
+{
+
+    private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+
+    public DiscountGrpcServices(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+    {
+        _discountProtoServiceClient = discountProtoServiceClient;
+    }
+
+    public async Task<CouponModel> GetDiscount(string productName)
+    {
+        var discountRequest = new GetDiscountRequest
+        {
+            ProductName = productName
+        };
+
+        return await _discountProtoServiceClient.GetDiscountAsync(discountRequest);
+    }
+}
+
