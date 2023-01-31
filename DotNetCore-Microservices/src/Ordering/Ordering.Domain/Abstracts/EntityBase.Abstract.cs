0d7cec86f588fe1b2ed1a8c7db3a86000874ab82
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-01-31 12:36:09 +0300

    add domain layer for ordering services

---
+++ DotNetCore-Microservices/src/Ordering/Ordering.Domain/Abstracts/EntityBase.Abstract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstracts
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string LastModified { get; set; }
+
+        public DateTime LastModifiedDate{ get; set; }
+    }
+}
