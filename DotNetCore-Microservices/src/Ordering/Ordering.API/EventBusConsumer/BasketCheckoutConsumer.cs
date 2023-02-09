978294467e8fb4bca2fede30b583f858ef2adc4d
majeed <majeed.dl@gmail.com>
2023-02-09 10:15:26 +0300

    Subscribe basketcheout queue

--- DotNetCore-Microservices/src/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
+++ DotNetCore-Microservices/src/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -1,12 +1,31 @@
+using AutoMapper;
 using EventBus.Messages.Events;
 using MassTransit;
+using MediatR;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 
 namespace Ordering.API.EventBusConsumer;
 
 public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
 {
-    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+    private readonly ILogger<BasketCheckoutConsumer> _logger;
+
+    public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
     {
-        throw new NotImplementedException();
+        _mapper = mapper;
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+
+        var result = await _mediator.Send(command);
+        
+        _logger.LogInformation("Basket Checkout Event consumed successfully");
     }
 }
